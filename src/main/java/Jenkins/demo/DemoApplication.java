5f31b061d54746b09cf0d23830132dc21b620ce6
youssefraqi <75301898+youssefraqi@users.noreply.github.com>
2021-01-01 17:59:33 -0500

    Update DemoApplication.java

--- src/main/java/Jenkins/demo/DemoApplication.java
+++ src/main/java/Jenkins/demo/DemoApplication.java
@@ -1,15 +1,34 @@
 package Jenkins.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 
 @SpringBootApplication
-public class DemoApplication{
+public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+
+    @GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
-	
+    
+
 }
